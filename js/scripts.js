f2e7eed6e6a22e0ea763672542d79c87bcf3c2fb
savespace <savespace@outlook.com>
2017-07-25 22:32:01 +0200

    Komentarz do skryptu

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+alert('W pliku scripts znajduje się kilka komentarzy blokowych i liniowych');
+
+//To jest pierwszy komentarz liniowy
+//alert('Przykładowy alert=wyłączony');
+//To jest drugi komentarz liniowy
+
+/*To jest pierwszy 
+komentarz blokowy*/
+/*alert(Przykładowy alert=wyłączony');*/
+
